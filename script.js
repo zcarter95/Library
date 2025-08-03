6dda83d3d788f5afc4f603835ad0ca91db8df049
Zach Carter <69975691+zcarter95@users.noreply.github.com>
2025-08-03 10:46:49 -0700

    change add book button functionality to hide/unhide form, rather than adding form to DOM, to prevent adding multiple forms to the page

--- script.js
+++ script.js
@@ -15,35 +15,12 @@ function addBook() {
 
 }
 function bringUpInput() {
-    let header = document.getElementsByClassName('header')[0];
-    let form = document.createElement('form');
-    form.className = 'new-book';
-    let formContents = `
-            <div class="input book-title">
-                <label for="title">Title</label>
-                <input type="text" id="title" name="book_title">
-            </div>
-            <div class="input book-author">
-                <label for="author">Author</label>
-                <input type="text" id="author" name="book_author">
-            </div>
-            <div class="input book-pages">
-                <label for="pages">Pages</label>
-                <input type="number" id="pages" name="book_pages">
-            </div>
-            <div class="input book-read">
-                <label for="read">Read?</label>
-                <select name="book_read" id="read">
-                    <optgroup>
-                        <option value="true">Yes</option>
-                        <option value="false">No</option>
-                    </optgroup>
-                </select>
-            </div>
-            <button type="submit">Add</button>
-        `
-        form.innerHTML = formContents;
-        header.append(form);
+    let form = document.getElementsByClassName('new-book-form')[0];
+    if (form.style.display === "none") {
+        form.style.display = null;
+    } else {
+        form.style.display = "none"
+    }
 }
 function updateLibrary() {
     myLibrary.forEach((book) => {
