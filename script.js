0560b09b4dccf52adce93e984be3de967c4db43a
Zach Carter <69975691+zcarter95@users.noreply.github.com>
2025-08-03 09:50:13 -0700

    add functionality for looping through myLibrary and adding cards to the DOM

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
+if (document.readyState == 'loading') {
+    document.addEventListener('DOMContentLoaded', ready)
+} else {
+    ready()
+}
+
+function ready() {
+    myLibrary.forEach((book) => {
+        let content = document.getElementsByClassName('content')[0];
+        let card = document.createElement("div");
+        card.className = 'card';
+        let cardContents = `
+                <h1>${book.name}</h1>
+                <ul>
+                    <li>Author: ${book.author}</li>
+                    <li>Pages:${book.pages}</li>
+                    <li>Read:${book.read}</li>
+                </ul>
+                <button class="remove">Remove</button>
+            `
+        card.innerHTML = cardContents;
+        content.append(card);
+    });
+}
+
 const myLibrary = [];
 
 function Book(id, name, author, pages, read) {
@@ -15,3 +40,5 @@ function addBookToLibrary(name, author, pages, read) {
 }
 
 addBookToLibrary("The Way of Kings", "Brandon Sanderson", 1007, true);
+addBookToLibrary("Harry Potter and the Sorcer's Stone", "J.K. Rowling", 500, true);
+
