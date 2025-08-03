6360c6c76b5a08d9801d26e25078c62cb0748177
Zach Carter <69975691+zcarter95@users.noreply.github.com>
2025-08-03 10:25:22 -0700

    add bring up form functionality

--- script.js
+++ script.js
@@ -5,6 +5,47 @@ if (document.readyState == 'loading') {
 }
 
 function ready() {
+    updateLibrary();
+    addBook();
+}
+
+function addBook() {
+    let add_book_button = document.getElementsByClassName('add-book')[0];
+    add_book_button.addEventListener('click', bringUpInput)
+
+}
+function bringUpInput() {
+    let header = document.getElementsByClassName('header')[0];
+    let form = document.createElement('form');
+    form.className = 'new-book';
+    let formContents = `
+            <div class="input book-title">
+                <label for="title">Title</label>
+                <input type="text" id="title" name="book_title">
+            </div>
+            <div class="input book-author">
+                <label for="author">Author</label>
+                <input type="text" id="author" name="book_author">
+            </div>
+            <div class="input book-pages">
+                <label for="pages">Pages</label>
+                <input type="number" id="pages" name="book_pages">
+            </div>
+            <div class="input book-read">
+                <label for="read">Read?</label>
+                <select name="book_read" id="read">
+                    <optgroup>
+                        <option value="true">Yes</option>
+                        <option value="false">No</option>
+                    </optgroup>
+                </select>
+            </div>
+            <button type="submit">Add</button>
+        `
+        form.innerHTML = formContents;
+        header.append(form);
+}
+function updateLibrary() {
     myLibrary.forEach((book) => {
         let content = document.getElementsByClassName('content')[0];
         let card = document.createElement("div");
@@ -22,7 +63,6 @@ function ready() {
         content.append(card);
     });
 }
-
 const myLibrary = [];
 
 function Book(id, name, author, pages, read) {
