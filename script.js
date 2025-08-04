997d089e47d08c6f567102ad6a9b403d6c9fae3a
Zach Carter <69975691+zcarter95@users.noreply.github.com>
2025-08-04 09:22:37 -0700

    add read button to cards

--- script.js
+++ script.js
@@ -1,86 +1,93 @@
-if (document.readyState == 'loading') {
-    document.addEventListener('DOMContentLoaded', ready)
+if (document.readyState == "loading") {
+  document.addEventListener("DOMContentLoaded", ready);
 } else {
-    ready()
+  ready();
 }
 
-
 function ready() {
-    let removeBookButtons = document.getElementsByClassName("remove");
-    for (var i = 0; i < removeBookButtons.length; i++) {
-        let button = removeBookButtons[i];
-        button.addEventListener('click', removeBook);
-    }
-    updateLibrary();
-    addBook();
+  let removeBookButtons = document.getElementsByClassName("remove");
+  for (var i = 0; i < removeBookButtons.length; i++) {
+    let button = removeBookButtons[i];
+    button.addEventListener("click", removeBook);
+  }
+  updateLibrary();
+  addBook();
 }
 
 function removeBook(event) {
-    var newLibrary = []
-    var buttonClicked = event.target;
-    myLibrary.forEach(book => {
-        if (book.id != buttonClicked.parentElement.id) {
-            newLibrary.push(book)
-        }
-    });
-    myLibrary = newLibrary;
-    buttonClicked.parentElement.remove();
+  var newLibrary = [];
+  var buttonClicked = event.target;
+  myLibrary.forEach((book) => {
+    if (book.id != buttonClicked.parentElement.id) {
+      newLibrary.push(book);
+    }
+  });
+  myLibrary = newLibrary;
+  buttonClicked.parentElement.remove();
 }
 
 function addBook() {
-    let add_book_button = document.getElementsByClassName('add-book')[0];
-    add_book_button.addEventListener('click', bringUpInput)
-    let submit = document.getElementsByClassName('new-book')[0];
-    submit.addEventListener("submit", function(event) {
-        event.preventDefault();
-        let formData = new FormData(submit);
-        addBookToLibrary(Object.fromEntries(formData).book_title, Object.fromEntries(formData).book_author, Object.fromEntries(formData).book_pages, Object.fromEntries(formData).book_read);
-        updateLibrary();
-        let form = document.getElementsByClassName('new-book-form')[0];
-        form.style.display = "none";
-        document.getElementsByClassName("new-book")[0].reset();
-    })
+  let add_book_button = document.getElementsByClassName("add-book")[0];
+  add_book_button.addEventListener("click", bringUpInput);
+  let submit = document.getElementsByClassName("new-book")[0];
+  submit.addEventListener("submit", function (event) {
+    event.preventDefault();
+    let formData = new FormData(submit);
+    addBookToLibrary(
+      Object.fromEntries(formData).book_title,
+      Object.fromEntries(formData).book_author,
+      Object.fromEntries(formData).book_pages,
+      Object.fromEntries(formData).book_read
+    );
+    updateLibrary();
+    let form = document.getElementsByClassName("new-book-form")[0];
+    form.style.display = "none";
+    document.getElementsByClassName("new-book")[0].reset();
+  });
 }
 function bringUpInput() {
-    let form = document.getElementsByClassName('new-book-form')[0];
-    if (form.style.display === "none") {
-        form.style.display = null;
-    } else {
-        form.style.display = "none"
-    }
+  let form = document.getElementsByClassName("new-book-form")[0];
+  if (form.style.display === "none") {
+    form.style.display = null;
+  } else {
+    form.style.display = "none";
+  }
 }
 function updateLibrary() {
-    myLibrary.forEach((book) => {
-        card_id = document.getElementById(book.id);
-        if (card_id) {
-            return;
-        }
-        let content = document.getElementsByClassName('content')[0];
-        let card = document.createElement("div");
-        card.className = 'card';
-        card.id = book.id;
-        let cardContents = `
+  myLibrary.forEach((book) => {
+    card_id = document.getElementById(book.id);
+    if (card_id) {
+      return;
+    }
+    let content = document.getElementsByClassName("content")[0];
+    let card = document.createElement("div");
+    card.className = "card";
+    card.id = book.id;
+    let cardContents = `
                 <h1>${book.name}</h1>
                 <ul>
                     <li>Author: ${book.author}</li>
                     <li>Pages:${book.pages}</li>
                     <li>Read:${book.read}</li>
                 </ul>
+                <button class="read">Read</button>
                 <button class="remove">Remove</button>
-            `
-        card.innerHTML = cardContents;
-        content.append(card);
-        card.getElementsByClassName("remove")[0].addEventListener('click', removeBook);
-    });
+            `;
+    card.innerHTML = cardContents;
+    content.append(card);
+    card
+      .getElementsByClassName("remove")[0]
+      .addEventListener("click", removeBook);
+  });
 }
 let myLibrary = [];
 
 function Book(id, name, author, pages, read) {
-    this.id = id;
-    this.name = name;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
+  this.id = id;
+  this.name = name;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
 }
 
 function addBookToLibrary(name, author, pages, read) {
@@ -88,5 +95,3 @@ function addBookToLibrary(name, author, pages, read) {
   let new_book = new Book(id, name, author, pages, read);
   myLibrary.push(new_book);
 }
-
-
